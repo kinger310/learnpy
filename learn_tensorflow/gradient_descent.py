ef9f072fd6b7ed7e4d4a843277eb9aa7c7e5e303
kinger310 <757441884@qq.com>
2017-09-10 23:27:09 +0800

    delete some data. tf

---
+++ learn_tensorflow/gradient_descent.py
@@ -0,0 +1,64 @@
+# Gradient Descent 梯度下降法
+# https://ctmakro.github.io/site/on_learning/gd.html
+# 已知 x0 * x1 + x0 * x2^2 + x1 + x1^3 + x2 + x2^5 + x3 + x3^7 - 15 = 0, 求 x0, x1, x2和x3.
+import numpy as np
+
+
+def problem(x):
+    return x[0] * x[1] + x[0] * (x[2] ** 2) + x[1] + x[1] ** 3 + x[2] + x[2] ** 5 + x[3] + x[3] ** 7 - 15
+
+
+def error(x):
+    return (problem(x) - 0) ** 2
+
+
+def gradient_descent(x, alpha=0.001):
+    delta = 0.00000001
+    gradient = np.zeros(x.shape)  # 梯度矢量
+
+    for i in range(len(gradient)):  # 逐个求取偏导数，放进梯度矢量
+        deltavector = np.zeros(x.shape)
+        deltavector[i] = delta
+        gradient[i] = (error(x + deltavector) - error(x - deltavector)) / (delta * 2)
+
+    x = x - gradient * alpha
+    return x
+
+
+def main():
+    x = np.array([0.0, 0.0, 0.0, 0.0])
+    for i in range(50):
+        x = gradient_descent(x, alpha=0.001)
+        print('i = {}, x = {}, problem(x) = {:6f}'.format(i, x, problem(x)))
+
+
+if __name__ == '__main__':
+    main()
+
+
+# 已知 x^3 + 2x + e^x - 3 = 0，求x。
+
+# def problem(x):
+#     # return x ** 3 + 2 * x + e ** x - 3
+#     return x ** 2 - 3
+#
+#
+# def error(x):
+#     return (problem(x) - 0) ** 2
+#
+#
+# def derivative_descent(x):
+#     delta = 0.00000001
+#     alpha = 0.01
+#     derivative = (error(x + delta) - error(x - delta)) / (delta * 2)
+#     # error 在 x 处的导数，这里是用导数的定义求的
+#     x = x - derivative * alpha
+#     return x
+#
+#
+# def main():
+#     x = 1
+#     # x = 0
+#     for i in range(100):
+#         x = derivative_descent(x)
+#         print('i={}, x = {:6f}, problem(x) = {:6f}'.format(i, x, problem(x)))
---
+++ learn_tensorflow/tf_example.py
@@ -0,0 +1,175 @@
+import tensorflow as tf
+
+hello = tf.constant('Hello, Tensorflow!')
+sess = tf.Session()
+print(sess.run(hello))
+
+node1 = tf.constant(3.0, dtype=tf.float32)
+node2 = tf.constant(4.0)
+print(node1, node2)
+sess = tf.Session()
+print(sess.run([node1, node2]))
+
+node3 = tf.add(node1, node2)
+print("node3:", node3)
+print("sess.run(node3):", sess.run(node3))
+
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+adder_node = a + b  # + provides a shortcut for tf.add(a, b)
+print(sess.run(adder_node, {a: 3, b: 4.5}))
+print(sess.run(adder_node, {a: [1, 3], b: [2, 4]}))
+
+W = tf.Variable([.3], dtype=tf.float32)
+b = tf.Variable([-.3], dtype=tf.float32)
+x = tf.placeholder(tf.float32)
+linear_model = W * x + b
+# Don't forget init
+init = tf.global_variables_initializer()
+sess.run(init)
+print(sess.run(linear_model, {x: [1, 2, 3, 4]}))
+
+y = tf.placeholder(tf.float32)
+squared_deltas = tf.square(linear_model - y)
+loss = tf.reduce_sum(squared_deltas)
+print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
+
+# from functools import reduce
+# reduce(lambda x, y: x + y,
+#        map(lambda x, y: (0.3 * x - 0.3 - y) ** 2,
+#            [1, 2, 3, 4], [0, -1, -2, -3]))
+
+fixW = tf.assign(W, [-1.])
+fixb = tf.assign(b, [1.])
+sess.run([fixW, fixb])
+print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
+
+#
+# Complete program
+#
+# The completed trainable linear regression model is shown here:
+import tensorflow as tf
+
+# Model parameters
+W = tf.Variable([.3], dtype=tf.float32)
+b = tf.Variable([-.3], dtype=tf.float32)
+# Model input and output
+x = tf.placeholder(tf.float32)
+linear_model = W * x + b
+y = tf.placeholder(tf.float32)
+
+# loss
+loss = tf.reduce_sum(tf.square(linear_model - y))  # sum of the loss squares
+# optimizer
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+
+# training data
+x_train = [1, 2, 3, 4]
+y_train = [0, -1, -2, -3]
+# training loop
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)  # reset values to wrong
+for i in range(1000):
+    sess.run(train, {x: x_train, y: y_train})
+
+# evaluate training accuracy
+curr_W, curr_b, curr_loss = sess.run([W, b, loss], {x: x_train, y: y_train})
+print("W: %s b: %s loss: %s" % (curr_W, curr_b, curr_loss))
+
+#
+#
+# Basic usage
+#
+# Notice how much simpler the linear regression program becomes with tf.estimator:
+
+import tensorflow as tf
+# NumPy is often used to load, manipulate and preprocess data.
+import numpy as np
+
+# Declare list of features. We only have one numeric feature. There are many
+# other types of columns that are more complicated and useful.
+feature_columns = [tf.feature_column.numeric_column("x", shape=[1])]
+
+# An estimator is the front end to invoke training (fitting) and evaluation
+# (inference). There are many predefined types like linear regression,
+# linear classification, and many neural network classifiers and regressors.
+# The following code provides an estimator that does linear regression.
+estimator = tf.estimator.LinearRegressor(feature_columns=feature_columns)
+
+# TensorFlow provides many helper methods to read and set up data sets.
+# Here we use two data sets: one for training and one for evaluation
+# We have to tell the function how many batches
+# of data (num_epochs) we want and how big each batch should be.
+x_train = np.array([1., 2., 3., 4.])
+y_train = np.array([0., -1., -2., -3.])
+x_eval = np.array([2., 5., 8., 1.])
+y_eval = np.array([-1.01, -4.1, -7, 0.])
+input_fn = tf.estimator.inputs.numpy_input_fn(
+    {"x": x_train}, y_train, batch_size=4, num_epochs=None, shuffle=True)
+train_input_fn = tf.estimator.inputs.numpy_input_fn(
+    {"x": x_train}, y_train, batch_size=4, num_epochs=1000, shuffle=False)
+eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+    {"x": x_eval}, y_eval, batch_size=4, num_epochs=1000, shuffle=False)
+
+# We can invoke 1000 training steps by invoking the  method and passing the
+# training data set.
+estimator.train(input_fn=input_fn, steps=1000)
+
+# Here we evaluate how well our model did.
+train_metrics = estimator.evaluate(input_fn=train_input_fn)
+eval_metrics = estimator.evaluate(input_fn=eval_input_fn)
+print("train metrics: %r" % train_metrics)
+print("eval metrics: %r" % eval_metrics)
+
+#
+# Custom
+#  df
+
+import numpy as np
+import tensorflow as tf
+
+
+# Declare list of features, we only have one real-valued feature
+def model_fn(features, labels, mode):
+    # Build a linear model and predict values
+    W = tf.get_variable("W", [1], dtype=tf.float64)
+    b = tf.get_variable("b", [1], dtype=tf.float64)
+    y = W * features['x'] + b
+    # Loss sub-graph
+    loss = tf.reduce_sum(tf.square(y - labels))
+    # Training sub-graph
+    global_step = tf.train.get_global_step()
+    optimizer = tf.train.GradientDescentOptimizer(0.01)
+    train = tf.group(optimizer.minimize(loss),
+                     tf.assign_add(global_step, 1))
+    # EstimatorSpec connects subgraphs we built to the
+    # appropriate functionality.
+    return tf.estimator.EstimatorSpec(
+        mode=mode,
+        predictions=y,
+        loss=loss,
+        train_op=train)
+
+
+estimator = tf.estimator.Estimator(model_fn=model_fn)
+# define our data sets
+x_train = np.array([1., 2., 3., 4.])
+y_train = np.array([0., -1., -2., -3.])
+x_eval = np.array([2., 5., 8., 1.])
+y_eval = np.array([-1.01, -4.1, -7, 0.])
+input_fn = tf.estimator.inputs.numpy_input_fn(
+    {"x": x_train}, y_train, batch_size=4, num_epochs=None, shuffle=True)
+train_input_fn = tf.estimator.inputs.numpy_input_fn(
+    {"x": x_train}, y_train, batch_size=4, num_epochs=1000, shuffle=False)
+eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+    {"x": x_eval}, y_eval, batch_size=4, num_epochs=1000, shuffle=False)
+
+# train
+estimator.train(input_fn=input_fn, steps=1000)
+# Here we evaluate how well our model did.
+train_metrics = estimator.evaluate(input_fn=train_input_fn)
+eval_metrics = estimator.evaluate(input_fn=eval_input_fn)
+print("train metrics: %r" % train_metrics)
+print("eval metrics: %r" % eval_metrics)
