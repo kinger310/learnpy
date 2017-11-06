e1411e519819d8ebd9f3cab8b6d22be78071d7c5
kinger310 <757441884@qq.com>
2017-11-06 14:57:41 +0800

    obp, developing...

--- obp/prod_order.py
+++ obp/prod_order.py
@@ -8,6 +8,7 @@
 
 import random
 import pandas as pd
+import numpy as np
 
 
 def prod_order():
@@ -35,10 +36,13 @@ def prod_order():
     return df
 
 
-def proc_time(df):
+def proc_time(df, para='s_shape'):
     num_item = len(df)
-    travel = s_shape(df)
-    proc_time = 3 + num_item / 6 + travel / 20
+    if para == 's_shape':
+        travel = s_shape(df)
+    else:
+        travel = large_gap(df)
+    proc_time = 3 + num_item / 4 + travel / 20
     return proc_time
 
 
@@ -56,14 +60,22 @@ def s_shape(df):
     return travel
 
 
+def large_gap(df):
+    travel = 1
+    return travel
+
 def main():
     # df_orders = prod_order()
     df_orders = pd.read_csv(r'./data/orders.csv')
     # 采用不同的Routing strategy会产生不同的路径
     # 采用S-shape策略，分奇数通道与偶数通道两种情况处理
-    x = df_orders.groupby(by=['order'])[['aisle', 'position']].apply(proc_time)
-
-
+    df_pt = df_orders.groupby(by=['order'])[['aisle', 'position']]\
+        .apply(lambda x: proc_time(x, para='s_shape')).reset_index(name='pt')
+    # modified traffic congestion rates
+    p_max, mtcr = 2, 0.6
+    min_pt, sum_pt = min(df_pt['pt']), sum(df_pt['pt'])
+    max_pt = (2 * (1 - mtcr) * sum_pt + min_pt) / p_max
+    df_pt['dt'] = [round(random.uniform(min_pt, max_pt), 2) for _ in range(len(df_pt))]
     print('ok')
 
 
