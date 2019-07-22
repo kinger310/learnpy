cd3014e5ff94d27f23d3e92bc899753bf4f4c10a
wangdi <wangdi03@ppdai.com>
2019-07-22 11:12:03 +0800

    try

--- leetcode/backtracking/gcd.py
+++ leetcode/backtracking/gcd.py
@@ -3,3 +3,14 @@ def gcd(x, y):
     return x if y == 0 else gcd(y, x%y)
 
 print(gcd(60, 576))
+
+
+# 扩展欧几里得算法
+def exgcd(a, b):
+    if b == 0:
+        return 1, 0, a
+    x, y, r = exgcd(b, a%b)
+    x, y = y, x - a//b * y
+    return x, y, r
+
+print(exgcd(17, 3120))
